b58a402aae1ee433496390697986720d3d80dc8e
mpanks <115006398+mpanks@users.noreply.github.com>
2023-11-01 15:21:37 +0000

    Removing outliers from dataset and outputting as a scatter graph

--- AI_CW.py
+++ AI_CW.py
@@ -1,4 +1,45 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-f=pd.read_csv('Data.csv')
-f.info()
+# Load data from file
+f=pd.read_csv('C:\\Users\\matth\\Downloads\\Data.csv')
+exampleData = f[["Lot Area","SalePrice"]]
+
+# exampleGraph = sns.load_dataset(exampleData)
+
+
+#calculatorStuff = exampleData["Lot Area"].to_numpy()
+
+# Splits 'exampledata' dict into two lists
+tempLotArea = exampleData["Lot Area"]
+tempSalePrice = exampleData["SalePrice"]
+landVal = []
+
+# calculates landvalue, area/price, adds to list
+for index in range(len(tempLotArea)):
+    lotArea = tempLotArea[index]
+    salePrice = tempSalePrice[index]
+    tempVal = lotArea/salePrice
+    landVal.append(tempVal)
+
+# Creates numpy list, calculates mean and std
+numpyLandVal = np.array(landVal)
+mean = numpyLandVal.mean()
+std = numpyLandVal.std()
+removed = []
+
+# itterates numpy list, removes outliers in dataset
+for index in range(len(numpyLandVal)):
+    if numpyLandVal[index]>=(mean+(3*std)):
+        removed.append(index)
+        exampleData = exampleData.drop(index)
+    elif numpyLandVal[index]<=(mean-(3*std)):
+       removed.append(index)
+       exampleData = exampleData.drop(index)
+
+# Outputs data as scatter graph
+graph = sns.FacetGrid(exampleData)
+graph.map(plt.scatter,"SalePrice","Lot Area",edgecolor = "Blue" ).add_legend()
+plt.show()
