4b861faba9b660bbf288d6ad47ad42b9e0ffd304
Matthew Panks <matthewpanksy@gmail.com>
2024-01-09 21:25:22 +0000

    Polynomial regression complete, cleaned up code and added regression line to linear model

--- AI_CW.py
+++ AI_CW.py
@@ -4,18 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
 # Load data from file
 allData=pd.read_csv('Data.csv', skip_blank_lines=True)
 
 # extract all data from Data.csv, except non-numerical features
+#Bsmt SF 1&2 and unf SF removed - covered by Total Bsmt SF
+#Bsmt Full&Half bath removed
+#Garage cars removed - covered by garage area
+#GarageYrBlt removed - unimportant
+#Misc Val removed - ambiguity
+#Bedroom AbvGr & Kitched AbvGr removed - covered by TotRms AbvGrd
 trimmedData = allData[["MS SubClass","Lot Frontage","Lot Area","Overall Qual","Overall Cond",
 "Year Built","Year Remod/Add","Mas Vnr Area",
-"BsmtFin SF 1","BsmtFin SF 2","Bsmt Unf SF","Total Bsmt SF","1st Flr SF","2nd Flr SF",
-"Low Qual Fin SF","Gr Liv Area","Bsmt Full Bath","Bsmt Half Bath",
-"Full Bath","Half Bath","Bedroom AbvGr","Kitchen AbvGr",
-"TotRms AbvGrd","Fireplaces","Garage Yr Blt","Garage Cars","Garage Area",
+"Total Bsmt SF","1st Flr SF","2nd Flr SF",
+"Low Qual Fin SF","Gr Liv Area",
+"Full Bath","Half Bath",
+"TotRms AbvGrd","Fireplaces","Garage Area",
 "Wood Deck SF","Open Porch SF", "Enclosed Porch",
-"3Ssn Porch","Screen Porch","Pool Area","Misc Val","Mo Sold","Yr Sold","SalePrice"]]
+"3Ssn Porch","Screen Porch","Pool Area","Mo Sold","Yr Sold","SalePrice"]]
 #print("Trimmed data:",trimmedData)
 
 #Replace null values with average for the column
@@ -31,50 +39,53 @@ for col in trimmedData:
 #2930 indexes, highest index = 2929
 #Training data = 0-2344
 #Test data = 2343-2929 
-trainingData = trimmedData.iloc[:2344]
-testData = trimmedData.tail(586)
 
-x_test= testData[["MS SubClass","Lot Frontage","Lot Area","Overall Qual","Overall Cond",
+X, Y = trimmedData[["MS SubClass","Lot Frontage","Lot Area","Overall Qual","Overall Cond",
 "Year Built","Year Remod/Add","Mas Vnr Area",
-"BsmtFin SF 1","BsmtFin SF 2","Bsmt Unf SF","Total Bsmt SF","1st Flr SF","2nd Flr SF",
-"Low Qual Fin SF","Gr Liv Area","Bsmt Full Bath","Bsmt Half Bath",
-"Full Bath","Half Bath","Bedroom AbvGr","Kitchen AbvGr",
-"TotRms AbvGrd","Fireplaces","Garage Yr Blt","Garage Cars","Garage Area",
+"Total Bsmt SF","1st Flr SF","2nd Flr SF",
+"Low Qual Fin SF","Gr Liv Area",
+"Full Bath","Half Bath",
+"TotRms AbvGrd","Fireplaces","Garage Area",
 "Wood Deck SF","Open Porch SF", "Enclosed Porch",
-"3Ssn Porch","Screen Porch","Pool Area","Misc Val","Mo Sold","Yr Sold"]]
-
-x_train=trainingData[["MS SubClass","Lot Frontage","Lot Area","Overall Qual","Overall Cond",
-"Year Built","Year Remod/Add","Mas Vnr Area",
-"BsmtFin SF 1","BsmtFin SF 2","Bsmt Unf SF","Total Bsmt SF","1st Flr SF","2nd Flr SF",
-"Low Qual Fin SF","Gr Liv Area","Bsmt Full Bath","Bsmt Half Bath",
-"Full Bath","Half Bath","Bedroom AbvGr","Kitchen AbvGr",
-"TotRms AbvGrd","Fireplaces","Garage Yr Blt","Garage Cars","Garage Area",
-"Wood Deck SF","Open Porch SF", "Enclosed Porch",
-"3Ssn Porch","Screen Porch","Pool Area","Misc Val","Mo Sold","Yr Sold"]]
-
-y_test= testData["SalePrice"]
-y_train=trainingData["SalePrice"]
+"3Ssn Porch","Screen Porch","Pool Area","Mo Sold","Yr Sold"]], trimmedData["SalePrice"]
 
 #Linear Regression
-
+xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2, random_state=42)
 linearModel = LinearRegression()
-linearModel.fit(x_train, y_train)
+linearModel.fit(xTrain, yTrain)
 
-predictions = linearModel.predict(x_test)
-plt.scatter(y_test, predictions)
-plt.savefig("./plots/linearRegressionPredictions.png")
-plt.clf()
+linPredict = linearModel.predict(xTest)
+fig=sns.regplot(x=yTest, y=linPredict)
+fig.figure.savefig("./plots/linearRegressionPredictions.png")
+fig.clear()
 
 ##Compute Mean Absolute Error
-print("Linear MAE: ", metrics.mean_absolute_error(y_test, predictions))
-
+print("Linear MAE: ", metrics.mean_absolute_error(yTest, linPredict))
 ##Compute Mean Squared Error
-print("Linear MSE: ", metrics.mean_squared_error(y_test,predictions))
-
+print("Linear MSE: ", metrics.mean_squared_error(yTest,linPredict))
 ##Compute Root Mean Squared Error
-print("Linear RMSE: ", np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+print("Linear RMSE: ", np.sqrt(metrics.mean_squared_error(yTest, linPredict)))
 
 #Polynomial Regresssion
+
+#Create and add data to model
+poly = PolynomialFeatures(degree=2,include_bias=True)
+features = poly.fit_transform(X)
+xTrain, xTest, yTrain, yTest = train_test_split(features, Y, test_size=0.2, random_state=42)
+
+polyRegModel = LinearRegression()
+polyRegModel.fit(xTrain,yTrain)
+polyPredict = polyRegModel.predict(xTest)
+
+#Computations
+print("Poly MAE: ",metrics.mean_absolute_error(yTest,polyPredict))
+print("Poly MSE: ", metrics.mean_squared_error(yTest,polyPredict))
+print("Poly RMSE: ",np.sqrt(metrics.mean_squared_error(yTest,polyPredict)))
+
+#Create and save seaborn figure
+fig = sns.regplot(x=yTest,y=polyPredict)
+fig.figure.savefig("./plots/PolyRegression")
+fig.clear()
 
 #Neural Network
 
