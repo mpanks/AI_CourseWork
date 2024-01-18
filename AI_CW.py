3bc6723de60c66bc108180b063ea073fa1d79fcf
Matthew Panks <matthewpanksy@gmail.com>
2024-01-18 16:32:56 +0000

    Modified data validation, test/training data sets are now only split once and modified figure outputs

--- AI_CW.py
+++ AI_CW.py
@@ -6,9 +6,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 # Load data from file
-allData=pd.read_csv('Data.csv', skip_blank_lines=True)
+df=pd.read_csv('Data.csv', skip_blank_lines=True)
 
 # extract all data from Data.csv, except non-numerical features
 #Bsmt SF 1&2 and unf SF removed - covered by Total Bsmt SF
@@ -17,88 +18,115 @@ allData=pd.read_csv('Data.csv', skip_blank_lines=True)
 #GarageYrBlt removed - unimportant
 #Misc Val removed - ambiguity
 #Bedroom AbvGr & Kitched AbvGr removed - covered by TotRms AbvGrd
-trimmedData = allData[["MS SubClass","Lot Frontage","Lot Area","Overall Qual","Overall Cond",
-"Year Built","Year Remod/Add","Mas Vnr Area",
-"Total Bsmt SF","1st Flr SF","2nd Flr SF",
-"Low Qual Fin SF","Gr Liv Area",
-"Full Bath","Half Bath",
-"TotRms AbvGrd","Fireplaces","Garage Area",
-"Wood Deck SF","Open Porch SF", "Enclosed Porch",
-"3Ssn Porch","Screen Porch","Pool Area","Mo Sold","Yr Sold","SalePrice"]]
-#print("Trimmed data:",trimmedData)
 
+FML: list[str] = [
+    "Order",
+    "Yr Sold",
+    "Mo Sold",
+    "BsmtFin SF 1",
+    "BsmtFin SF 2",
+    "1st Flr SF",
+    "2nd Flr SF",
+    "Low Qual Fin SF",
+    "Bsmt Full Bath",
+    "Bsmt Half Bath",
+    "Full Bath",
+    "Half Bath",
+    "Bedroom AbvGr",
+    "Kitchen AbvGr",
+]
+df.drop(FML, axis=1)
+non_numeric_columns: list[str] = df.select_dtypes(
+    exclude=np.number).columns.tolist()
+
+df = df.drop(non_numeric_columns, axis = 1)
 #Replace null values with average for the column
 
-for col in trimmedData:
+df = df.fillna(df.mean())
 
-    currentCol = trimmedData[col]
-    mean = float
-    mean = currentCol.mean()
-    trimmedData[col] = trimmedData[col].fillna(mean)
+# Scale columns except last (sale price)
+scaler=MinMaxScaler()
+df[df.columns[:-1]] = scaler.fit_transform(df[df.columns[:-1]])
 
-#Partition dataset 80/20 for training & testingpytho
-#2930 indexes, highest index = 2929
+#Partition dataset 80/20 for training & testing
 #Training data = 0-2344
 #Test data = 2343-2929 
+df_train: pd.DataFrame = df.iloc[: int(0.8*len(df))]
+df_test: pd.DataFrame = df.iloc[int(0.8*len(df)):]
 
-X, Y = trimmedData[["MS SubClass","Lot Frontage","Lot Area","Overall Qual","Overall Cond",
-"Year Built","Year Remod/Add","Mas Vnr Area",
-"Total Bsmt SF","1st Flr SF","2nd Flr SF",
-"Low Qual Fin SF","Gr Liv Area",
-"Full Bath","Half Bath",
-"TotRms AbvGrd","Fireplaces","Garage Area",
-"Wood Deck SF","Open Porch SF", "Enclosed Porch",
-"3Ssn Porch","Screen Porch","Pool Area","Mo Sold","Yr Sold"]], trimmedData["SalePrice"]
+df_x_train: pd.DataFrame = df_train.drop(["SalePrice"],axis=1)
+df_y_train: pd.DataFrame = df_train["SalePrice"]
+
+df_x_test: pd.DataFrame = df_test.drop(["SalePrice"],axis=1)
+df_y_test: pd.DataFrame = df_test["SalePrice"]
+
 
 #Linear Regression
-xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2, random_state=42)
 linearModel = LinearRegression()
-linearModel.fit(xTrain, yTrain)
+linearModel.fit(df_x_train, df_y_train)
 
-linPredict = linearModel.predict(xTest)
-fig=sns.regplot(x=yTest, y=linPredict)
-fig.figure.savefig("./plots/linearRegressionPredictions.png")
-fig.clear()
+linPredict = linearModel.predict(df_x_test)
+#fig=sns.regplot(x=df_y_test, y=linPredict)
+plt.scatter(df_y_test,linPredict)
+plt.plot(df_y_test, np.poly1d(np.polyfit(df_y_test,linPredict,1))(df_y_test),color="red")
+#fig.set(xlabel='True Value',ylabel='Predicted Value')
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.savefig("./plots/linearRegressionPredictions.png")
+plt.clf()
 
 ##Compute Mean Absolute Error
-print("Linear MAE: ", metrics.mean_absolute_error(yTest, linPredict))
+print("Linear MAE: ", metrics.mean_absolute_error(df_y_test, linPredict))
 ##Compute Mean Squared Error
-print("Linear MSE: ", metrics.mean_squared_error(yTest,linPredict))
+print("Linear MSE: ", metrics.mean_squared_error(df_y_test,linPredict))
 ##Compute Root Mean Squared Error
-print("Linear RMSE: ", np.sqrt(metrics.mean_squared_error(yTest, linPredict)))
+print("Linear RMSE: ", np.sqrt(metrics.mean_squared_error(df_y_test, linPredict)))
 
 #Polynomial Regresssion
 
 #Create and add data to model
-poly = PolynomialFeatures(degree=2,include_bias=True)
-features = poly.fit_transform(X)
-xTrain, xTest, yTrain, yTest = train_test_split(features, Y, test_size=0.2, random_state=42)
-
 polyRegModel = LinearRegression()
-polyRegModel.fit(xTrain,yTrain)
-polyPredict = polyRegModel.predict(xTest)
+poly = PolynomialFeatures(degree=2,include_bias=False)
+features = poly.fit_transform(df_x_train)
+
+polyRegModel.fit(df_x_train,df_y_train)
+polyPredict = polyRegModel.predict(df_x_test)
 
 #Computations
-print("Poly MAE: ",metrics.mean_absolute_error(yTest,polyPredict))
-print("Poly MSE: ", metrics.mean_squared_error(yTest,polyPredict))
-print("Poly RMSE: ",np.sqrt(metrics.mean_squared_error(yTest,polyPredict)))
+print("Poly MAE: ",metrics.mean_absolute_error(df_y_test,polyPredict))
+print("Poly MSE: ", metrics.mean_squared_error(df_y_test,polyPredict))
+print("Poly RMSE: ",np.sqrt(metrics.mean_squared_error(df_y_test,polyPredict)))
 
-#Create and save seaborn figure
-fig = sns.regplot(x=yTest,y=polyPredict)
-fig.figure.savefig("./plots/PolyRegression")
-fig.clear()
+#Create and save figure
+
+plt.scatter(df_y_test, polyPredict)
+plt.plot(df_y_test, np.poly1d(np.polyfit(df_y_test,polyPredict,1))(df_y_test),color="red")
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.savefig("./plots/PolyRegression")
+plt.clf()
 
 #Neural Network
-X_new, X_test, y_new, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-dev_per = X_test.shape[0]/X_new.shape[0]
-X_train, X_dev, y_train, y_dev = train_test_split(X_new, y_new, test_size=dev_per, random_state=0)
+df = pd.concat([df_train,df_test])
+df_train = df.iloc[:int(df.shape[0]*0.6)]
+df_val = df.iloc[int(df.shape[0]*0.6):int(df.shape[0]*0.8)]
+df_test = df.iloc[int(df.shape[0]*0.8) :]
+
+df_x_train: pd.DataFrame = df_train.drop(["SalePrice"],axis=1)
+df_y_train: pd.DataFrame = df_train["SalePrice"]
+
+df_x_test: pd.DataFrame = df_test.drop(["SalePrice"],axis=1)
+df_y_test: pd.DataFrame = df_test["SalePrice"]
+
+df_x_val: pd.DataFrame = df_val.drop(["SalePrice"],axis=1)
+df_y_val: pd.DataFrame = df_val["SalePrice"]
 
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
-model = Sequential([
-    Dense(128, activation='relu', input_shape=(26,)),
+network = Sequential([
+    Dense(128, activation='relu', input_shape=(38,)),
     Dense(128, activation='relu'),
     Dense(128, activation='relu'),
     Dense(128, activation='relu'),
@@ -108,17 +136,20 @@ model = Sequential([
 
 optimizer = Adam(learning_rate=0.001)
 
-model.compile(optimizer= optimizer,
+network.compile(optimizer= optimizer,
               loss='mean_squared_error',
               metrics=['mean_squared_error'])
 
-hist = model.fit(X_train, y_train,
-          batch_size=0, epochs=128,
-          validation_data=(X_dev, y_dev),
+hist = network.fit(df_x_train, df_y_train,
+          batch_size=8, epochs=128,
+          validation_data=(df_x_val, df_y_val),
           verbose = 0)
 
+NNetPredictions: np.ndarray = network.predict(df_x_test)
 
 #Loss of NNet
+plt.clf()
+plt.title("NNet Loss")
 plt.plot(hist.history['loss'])
 plt.plot(hist.history['val_loss'])
 plt.title('Model loss')
@@ -135,53 +166,19 @@ plt.plot(hist.history['val_mean_squared_error'])
 plt.title('Model accuracy')
 plt.ylabel('MSE')
 plt.xlabel('Epoch')
-plt.legend(['Train', 'Val'], loc='lower right')
+plt.legend(['Train', 'Val'], loc='upper right')
 plt.savefig("./plots/NNetAccuracy.png")
 plt.clf()
 
+# Plot predictions against true values 
+plt.scatter(df_y_test, NNetPredictions)
+y_pred = NNetPredictions.flatten()
+plt.plot(df_y_test,np.poly1d(np.polyfit(df_y_test,y_pred,1))(df_y_test), color="red")
+plt.savefig("./plots/NNetPredictions.png")
+plt.clf()
+
 # Evaluate the model on the test set
-test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=1)
+test_loss, test_accuracy = network.evaluate(df_x_test, df_y_test, verbose=1)
 # Print the accuracy
 print(f"Test MSE: {test_accuracy}")
 print(f"Val Loss: {test_loss}")
-
-# exampleGraph = sns.load_dataset(exampleData)
-#calculatorStuff = exampleData["Lot Area"].to_numpy()
-# Splits 'exampledata' dict into two lists
-
-#tempLotArea = exampleData["Lot Area"]
-#tempSalePrice = exampleData["SalePrice"]
-#landVal = []
-
-# calculates landvalue, area/price, adds to list
-#for index in range(len(tempLotArea)):
- #   lotArea = tempLotArea[index]
-  #  salePrice = tempSalePrice[index]
-   # tempVal = lotArea/salePrice
-    #landVal.append(tempVal)
-
-# Creates numpy list, calculates mean and std
-#numpyLandVal = np.array(landVal)
-#mean = numpyLandVal.mean()
-#std = numpyLandVal.std()
-#removed = []
-
-# itterates numpy list, removes outliers in dataset
-#for index in range(len(numpyLandVal)):
-#    if numpyLandVal[index]>=(mean+(3*std)):
-#        removed.append(index)
-#        exampleData = exampleData.drop(index)
-#    elif numpyLandVal[index]<=(mean-(3*std)):
-#       removed.append(index)
-#       exampleData = exampleData.drop(index)
-
-# Outputs data as scatter graph
-#graph = sns.FacetGrid(exampleData)
-#graph.map(plt.scatter,"SalePrice","Lot Area",edgecolor = "Blue" ).add_legend()
-
-# Generates line of best fit, adds to graph before display
-#model = np.poly1d(np.polyfit(exampleData["SalePrice"],exampleData["Lot Area"],1))
-#polyline = np.linspace(exampleData.min(), exampleData.max(), 50)
-#plt.plot(polyline, model(polyline))
-#plt.savefig("./plots/test.png")
-#plt.clf()
